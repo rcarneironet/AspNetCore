6a0abd4951648d179c04da3b883e31c7f722513d
Justin Kotalik <jukotali@microsoft.com>
2019-02-14 18:15:58 -0800

    bunch of tests

--- src/Servers/Kestrel/Core/src/Internal/Http/HttpProtocol.cs
+++ src/Servers/Kestrel/Core/src/Internal/Http/HttpProtocol.cs
@@ -1230,6 +1230,11 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http
 
         public void ReportApplicationError(Exception ex)
         {
+            if (ex == null)
+            {
+                return;
+            }
+
             if (_applicationException == null)
             {
                 _applicationException = ex;
